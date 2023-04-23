3e470a9ce148ab70bf96977affbf1d7e3e6f1f20
Hassanamiri <hassanamiri369@gmail.com>
2023-04-24 01:57:25 +0330

    adding themeSlice

--- app/src/store.tsx
+++ app/src/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import todoSlice from "./features/todoSlice";
+import themeSlice from "./features/themeSlice";
 
 export const store = configureStore({
     reducer : {
-        todos : todoSlice
+        todos : todoSlice , 
+        theme : themeSlice
     }
 })
 
